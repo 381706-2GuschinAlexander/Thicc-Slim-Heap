2d255af7bcb92d4d6ff952b038274b8e6fe2d55d
Alexander Guschin <igushin1999@gmail.com>
2020-06-02 16:23:38 +0300

    added geters

--- link_lib/link.cpp
+++ link_lib/link.cpp
@@ -1,10 +1,43 @@
 #include "link.h"
 
-CoreNode::CoreNode() {
+Node::Node() {
   key = rank = -1;
-  left = right = child  = nullptr;
+  left_ptr = right_ptr = child_ptr  = nullptr;
 }
 
-CoreNode::CoreNode(int64_t key_v, int64_t rank_v, CoreNode* left_p,
-                   CoreNode* right_p, CoreNode* child_p)
-    : key(key_v), rank(rank_v), left(left_p), right(right_p), child(child_p) {}
+Node::Node(int64_t key_v, int64_t rank_v, Node* left_p,
+                   Node* right_p, Node* child_p)
+    : key(key_v),
+      rank(rank_v),
+      left_ptr(left_p),
+      right_ptr(right_p),
+      child_ptr(child_p) {}
+
+Node& Node::operator=(const Node& node) {
+  key = node.key;
+  rank = node.rank;
+  left_ptr = node.left_ptr;
+  right_ptr = node.right_ptr;
+  child_ptr = node.child_ptr;
+}
+
+void Node::SetKey(int64_t value) { key = value; }
+
+void Node::SetRank(int64_t value) { rank = value; }
+
+void Node::SetLeft(Node* pointer) { left_ptr = pointer; }
+
+void Node::SetRight(Node* pointer) { right_ptr = pointer; }
+
+void Node::SetChild(Node* pointer) { child_ptr = pointer; }
+
+int64_t Node::GetKey() { return key; }
+
+int64_t Node::GetRank() { return rank; }
+
+Node* Node::GetLeft() { return left_ptr; }
+
+Node* Node::GetRight() { return right_ptr; }
+
+Node* Node::GetChild() { return child_ptr; }
+
--- link_lib/link.h
+++ link_lib/link.h
@@ -15,12 +15,20 @@ class Node {
   Node();
   Node(int64_t key_v, int64_t rank_v, Node* left_p, Node* right_p,
            Node* child_p);
+
+  Node& operator=(const Node& node);
+
   void SetKey(int64_t value);
   void SetRank(int64_t value);
   void SetLeft(Node* pointer);
   void SetRight(Node* pointer);
   void SetChild(Node* pointer);
 
+  int64_t GetKey();
+  int64_t GetRank();
+  Node* GetLeft();
+  Node* GetRight();
+  Node* GetChild();
 };
 
 
